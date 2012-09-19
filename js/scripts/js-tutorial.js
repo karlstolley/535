067c45a97609f2682173a7fc988e02f2a85ba75d
Matthew Heston <matthewheston@gmail.com>
2012-09-18 19:08:12 -0500

    Make a button to undo the annoying background color in js tutorial.

--- js/scripts/js-tutorial.js
+++ js/scripts/js-tutorial.js
@@ -1,3 +1,11 @@
 function getRandomColor() {
   return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
+
+function removeAwfulBackgroundColor() {
+  document.body.style.background = "#FFFFFF";
+}
+
+document.getElementById("make-not-annoying-button").onclick = function() {
+  removeAwfulBackgroundColor();
+};
