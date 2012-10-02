606aa22a1bd0407774f509040599560ec7c26546
Anthony Sansone <github@anthony-sansone.com>
2012-10-01 23:33:49 -0500

    Created Ruby CSV to HTML transform script called file_read.rb. For conversation and discussion. Needs classes and possibly hashes.

---
+++ ruby/file_read.rb
@@ -0,0 +1,62 @@
+def read_csv(test_file)
+
+  class_file = Array.new
+  
+  begin 
+    file = File.new(test_file, "r")
+  rescue
+    puts "What file do you want to read?"
+    test_file = gets.chomp
+    retry
+  end
+
+  while (line = file.gets)
+    class_file.push(line)
+  end
+
+  class_file
+
+end
+
+def parse_csv(class_file)
+
+  course_list = Array.new
+  courses = class_file.length
+  i=0
+  while i < courses do
+    course_list.push(class_file[i].split(","))
+    i+=1
+  end
+
+  course_list
+
+end
+
+def convert_csv(csv_data,html_filename)
+  begin 
+    html_file = File.open(html_filename, "w")
+  rescue
+    puts "What do you want to call the HTML file?"
+    html_filename = gets.chomp
+    retry
+  end
+    html_file.puts "<table>"
+    table_row = 0
+    while table_row < csv_data.length do
+      html_file.puts "  <tr>"
+      table_cell = 0
+      while table_cell < csv_data[table_row].length
+        html_file.puts "    <td>" + (csv_data[table_row][table_cell]).chomp + "</td>"
+        table_cell += 1
+      end
+      html_file.puts "  </tr>"
+      table_row += 1
+    end
+    html_file.puts "</table>"
+end
+
+csv_raw_file = 'test.csv'
+html_file = 'test.htm'
+csv_file = read_csv(csv_raw_file)
+course_list = parse_csv(csv_file)
+convert_csv(course_list,html_file)
