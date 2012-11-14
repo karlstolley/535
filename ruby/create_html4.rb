b8343a4e75c91a7c054a0843326768de7dd5b02e
Anthony Sansone <asansone@dailycardinal.net>
2012-11-13 20:58:32 -0600

    Made changes to add Previous / Next and Progress bar

--- ruby/create_html4.rb
+++ ruby/create_html4.rb
@@ -1,4 +1,4 @@
-# Version 5: Writes to a Separate HTML File
+# Version 4: Writes to a Separate HTML File
 
 class CreateHTML
 
@@ -15,33 +15,33 @@ class CreateHTML
 
     print "\nHow many classes do you have this semester? "
     @course_count = gets.chomp
-    
+
 		@courses_data = []
-    
+
     @courses = (@course_count.to_i)-1
-    
+
     0.upto(@courses) do |table_row|
 
 			@courses_data[table_row] = {}
-			
+
       table_list_row = (table_row + 1).to_s
-      
-			questions.each_pair { |question,key| 
+
+			questions.each_pair { |question,key|
 				print "\n#{question} #{table_list_row}? "
 				@courses_data[table_row].merge!(key => gets.chomp)
 			}
 
       table_row += 1
-			
+
     end
 
   end
 
 
   def build_html_file
-  
+
   	html_filename = ""
-  
+
     begin
       html_file = File.open(html_filename, "w")
     rescue
@@ -49,15 +49,15 @@ class CreateHTML
       html_filename = gets.chomp + ".html"
       retry
     end
-    
+
     html_file.puts "<table>"
 
     @courses_data.each do |table_row|
 
 			html_file.puts "  <tr>"
-      
+
       table_row.each_value { |table_cell| html_file.puts "    <td>" + table_cell + "</td>" }
-      
+
       html_file.puts "  </tr>"
 
     end
@@ -73,3 +73,4 @@ end
 create_new_HTML = CreateHTML.new
 create_new_HTML.ask_questions
 create_new_HTML.build_html_file
+
